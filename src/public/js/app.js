221fbbdb908006b298197d7f1130a66524801669
BSH <bshyeon1217@gmail.com>
2024-08-27 02:55:19 +0900

    forntend/backend send back message

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,18 @@
-alert("hi");
+const socket = new WebSocket('ws://${window.location.host}');
+
+//3 event
+socket.addEventListener("open", () => {
+    console.log("Connected to Server ✅")
+}); 
+
+socket.addEventListener("message", (message) => {
+    console.log("New message: ", message.data, "from the Server")
+});
+
+socket.addEventListener("close", () => {
+    console.log("Disconnected from Server ❌");
+});
+
+setTimeout(() => {
+    socket.send("hello from the browser!");
+}, 10000);
