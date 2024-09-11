b3471069d60c4ffebdef07667ce79fcbc2f3d22c
BSH <bshyeon1217@gmail.com>
2024-09-12 02:26:49 +0900

    Admin UI Panel!

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import http from "http";
-import SocketIO from "socket.io";
+import { Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
 import express from "express";
-import { nextTick } from "process";
 
 const app = express();
 
@@ -12,7 +12,15 @@ app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
 const httpServer = http.createServer(app);
-const wsServer = SocketIO(httpServer);
+const wsServer = new Server(httpServer, {
+    cors: {
+      origin: ["https://admin.socket.io"],
+      credentials: true,
+    },
+});
+instrument(wsServer, {
+    auth: false,
+});
 
 function publicRooms() {
     const {
