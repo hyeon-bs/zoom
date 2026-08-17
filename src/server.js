2d531e1a034ccc4dcef2e31bde15f6e0099e4237
sohyeon <bshyeon1217@gmail.com>
2026-08-17 20:03:37 +0900

    feat: TURN  서버 연동으로 다른 네트워크 간 WebRTC 연결을 지원 - dotenv 패키지 추가 및 .env에서 TURN 키 설정 - server.js에서 TURN 환경변수를 pug 템플릿으로 전달 - home.pug에서 window.turnConfig 초기화 - app.js RTCPeerConnection iceServers에 TURN 서버 추가

--- src/server.js
+++ src/server.js
@@ -3,12 +3,21 @@ import express from "express";
 import { Server } from "socket.io";
 import { instrument } from "@socket.io/admin-ui";
 
+require("dotenv").config();
+
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (_, res) => res.render("home"));
+//app.get("/", (_, res) => res.render("home"));
+app.get("/", (req, res) => {
+    res.render("home", {
+        turnUrl: process.env.TURN_URL,
+        turnUsername: process.env.TURN_USERNAME,
+        turnCredential: process.env.TURN_CREDENTIAL
+    });
+});
 app.get("/*", (_, res) => res.redirect("/"));
 
 const httpServer = http.createServer(app);
